37b1361615b0e2bc928c393884cbfd5c71af1e53
MamtaPandit <114858932+MamtaPandit@users.noreply.github.com>
2022-10-10 11:28:51 +0530

    Add adaptive design for navigation

---
+++ lib/views/screens/adaptive_navigation.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class AdaptiveNavigation extends StatelessWidget {
+  const AdaptiveNavigation({
+    Key? key,
+    required this.destinations,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+    required this.child,
+  }) : super(key: key);
+
+  final List<NavigationDestination> destinations;
+  final int selectedIndex;
+  final void Function(int index) onDestinationSelected;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, dimens) {
+        // Tablet Layout
+        if (dimens.maxWidth >= 600) { // Add this line
+          return Scaffold(
+            body: Row(
+              children: [
+                NavigationRail(
+                  extended: dimens.maxWidth >= 800,
+                  minExtendedWidth: 180,
+                  destinations: destinations
+                      .map((e) => NavigationRailDestination(
+                            icon: e.icon,
+                            label: Text(e.label),
+                          ))
+                      .toList(),
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: onDestinationSelected,
+                ),
+                Expanded(child: child),
+              ],
+            ),
+          );
+        } // Add this line
+
+        // Mobile Layout
+        // Add from here...
+        return Scaffold(
+          body: child,
+          bottomNavigationBar: NavigationBar(
+            destinations: destinations,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: onDestinationSelected,
+          ),
+        );
+        // ... To here.
+      },
+    );
+  }
+}
